c24e22fb086c27d4bded70eab0d1c88a129e2ad9
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-03 15:45:46 +0200

    add Button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
 import calculator from "./utils/Calculations";
 import validations from "./utils/Validations";
 
@@ -39,115 +40,26 @@ function App() {
       <div className="border-2 border-solid border-black grid text-3xl grid-cols-4 grid-rows-5 divide-x-2 divide-y-2 w-[32rem] h-[44rem]">
         <button
           className="h-full w-full bg-orange-500 col-span-2 hover:bg-orange-600 transition-all font-bold text-white"
-          onClick={(e) => setExpression("")}
+          onClick={() => setExpression("")}
         >
           AC
         </button>
-        {/* TODO: for later implementation */}
-        {/* <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-orange-500 hover:bg-red-600 transition-all font-bold text-white"
-        >
-          +/-
-        </button> */}
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-orange-500 hover:bg-orange-600 transition-all font-bold text-white"
-        >
-          %
-        </button>
-        <button
-          className="h-full w-full bg-orange-500 hover:bg-orange-600 transition-all font-bold text-white"
-          onClick={(e) => addToExpression(e.target.textContent)}
-        >
-          /
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          7
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          8
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          9
-        </button>
-
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-orange-500 hover:bg-orange-600 transition-all font-bold text-white"
-        >
-          *
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          4
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          5
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          6
-        </button>
-
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-orange-500 hover:bg-orange-600 transition-all font-bold text-white"
-        >
-          -
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          1
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          2
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          3
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-orange-500 hover:bg-orange-600 transition-all font-bold text-white"
-        >
-          +
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all col-span-2"
-        >
-          0
-        </button>
-        <button
-          onClick={(e) => addToExpression(e.target.textContent)}
-          className="h-full w-full bg-gray-300 hover:bg-gray-400 transition-all"
-        >
-          .
-        </button>
+        <Button value="%" onClick={addToExpression} type="sign" />
+        <Button value="/" onClick={addToExpression} type="sign" />
+        <Button value="7" onClick={addToExpression} />
+        <Button value="8" onClick={addToExpression} />
+        <Button value="9" onClick={addToExpression} />
+        <Button value="*" onClick={addToExpression} type="sign" />
+        <Button value="4" onClick={addToExpression} />
+        <Button value="5" onClick={addToExpression} />
+        <Button value="6" onClick={addToExpression} />
+        <Button value="-" onClick={addToExpression} type="sign" />
+        <Button value="1" onClick={addToExpression} />
+        <Button value="2" onClick={addToExpression} />
+        <Button value="3" onClick={addToExpression} />
+        <Button value="+" onClick={addToExpression} type="sign" />
+        <Button value="0" onClick={addToExpression} />
+        <Button value="." onClick={addToExpression} />
         <button
           onClick={() => {
             setExpression(
@@ -156,7 +68,7 @@ function App() {
                 : calculator.evaluate(expression)
             );
           }}
-          className="h-full w-full bg-orange-500 hover:bg-orange-600 transition-all font-bold text-white "
+          className="h-full w-full bg-orange-500 hover:bg-white hover:text-orange-600 transition-all col-span-2 font-bold text-white "
         >
           =
         </button>
