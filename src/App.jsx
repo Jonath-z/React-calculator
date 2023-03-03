f9e2cb5f7b6c01587f87e471f5aaa2f7abf137f9
Jonath-z <jonathanzihindula95@gmail.com>
2023-03-03 16:28:24 +0200

    add deletion and previous calculation memorization

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import Button from "./components/Button";
 import calculator from "./utils/Calculations";
 import validations from "./utils/Validations";
+import { RiDeleteBack2Fill } from "react-icons/ri";
 
 function App() {
   const [expression, setExpression] = useState("");
+  const [answer, setAnswer] = useState("");
 
   function addToExpression(value) {
+    setAnswer("");
     setExpression((prev) => {
+      let newValue = `${prev}${value}`;
       if (prev.length <= 8) {
-        let newValue = prev + value;
-
         if (
           !validations.hasTwoZerosAtTheBegining(newValue) &&
           !validations.hasStartedWithAdot(newValue) &&
@@ -32,17 +34,27 @@ function App() {
   return (
     <div className="w-[32rem] h-[44rem] mx-auto">
       <div className="flex flex-col justify-start py-10 overscroll-y-contain">
-        <input
-          value={expression}
-          className="text-8xl m-0 p-0 min-h-[6rem] transition-all bg-gray-600 text-white rounded-sm"
-        />
+        <div className="text-8xl m-0 p-0 min-h-[6rem] transition-all bg-gray-600 text-white rounded-sm">
+          {answer || expression}
+        </div>
       </div>
       <div className="border-2 border-solid border-black grid text-3xl grid-cols-4 grid-rows-5 divide-x-2 divide-y-2 w-[32rem] h-[44rem]">
         <button
-          className="h-full w-full bg-orange-500 col-span-2 hover:bg-orange-600 transition-all font-bold text-white"
-          onClick={() => setExpression("")}
+          className="h-full w-full bg-orange-500 hover:bg-red-600 transition-all font-bold text-white"
+          onClick={() => {
+            setExpression("");
+            setAnswer("");
+          }}
         >
           AC
+        </button>
+        <button
+          className="h-full w-full bg-orange-500 flex justify-center items-center hover:bg-red-600 transition-all font-bold text-white"
+          onClick={() => {
+            setExpression((prev) => prev.slice(0, -1));
+          }}
+        >
+          <RiDeleteBack2Fill />
         </button>
         <Button value="%" onClick={addToExpression} type="sign" />
         <Button value="/" onClick={addToExpression} type="sign" />
@@ -62,10 +74,10 @@ function App() {
         <Button value="." onClick={addToExpression} />
         <button
           onClick={() => {
-            setExpression(
+            setAnswer(
               isNaN(calculator.evaluate(expression))
                 ? "E"
-                : calculator.evaluate(expression)
+                : calculator.evaluate(String(expression))
             );
           }}
           className="h-full w-full bg-orange-500 hover:bg-white hover:text-orange-600 transition-all col-span-2 font-bold text-white "
